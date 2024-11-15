f53c2aa537061647f6ac5617896eebd912000d8e
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-11-15 21:37:36 +0900

    Feat: 시간 선택 모달 구현

--- app/components/writeTripSchedule/WriteTimeline.tsx
+++ app/components/writeTripSchedule/WriteTimeline.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { FlatList } from 'react-native';
-
+import DatePicker from 'react-native-date-picker';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const WriteTimeline: React.FC = () => {
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [selectedTime, setSelectedTime] = useState(new Date());
+
+    // const showDatePicker = () => {
+    //     setDatePickerVisibility(true);
+    // };
+
+    // const hideDatePicker = () => {
+    //     setDatePickerVisibility(false);
+    // };
+
+    // const handleConfirm = (date: any) => {
+    //     console.warn("A date has been picked: ", date);
+    //     hideDatePicker();
+    // };
     return (
         <View style={styles.screen}>
-            <Text>시간</Text>
-            <TextInput style={styles.inputStyle}/>
+            <TouchableOpacity onPress={()=>setDatePickerVisibility(true)}>
+                <Ionicons name='time-outline' size={15} color={'#000000'}/>
+                <DatePicker 
+                modal
+                open={isDatePickerVisible}
+                mode='time'
+                date={selectedTime}
+                onConfirm={(date) => {
+                    setDatePickerVisibility(true)
+                    setSelectedTime(date);
+                  }}
+                  onCancel={() => {
+                    setDatePickerVisibility(false)
+                  }}
+                />
+            </TouchableOpacity>
             <Text>일정 제목</Text>
             <TextInput style={styles.inputStyle}/>
             <Text>일정 내용</Text>
