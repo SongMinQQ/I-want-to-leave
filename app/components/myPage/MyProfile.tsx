7546156700b3bb949c5af853f771c9ecade9de89
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-11-18 21:43:23 +0900

    Feat: 내 프로필 프로토타입 구현

--- app/components/myPage/MyProfile.tsx
+++ app/components/myPage/MyProfile.tsx
@@ -1,13 +1,88 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import basicProfile from '../../assets/basicProfile.png';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const { width: deviceWidth } = Dimensions.get('window');
 
 const MyProfile: React.FC = () => {
+    const [isEdit, setIsEdit] = useState<Boolean>(false);
+
     return (
-        <View>
+        <View style={styles.screen}>
+            {/* Edit Button */}
+            <TouchableOpacity 
+                style={styles.editButton} 
+                onPress={() => setIsEdit(true)}>
+                <AntDesign name="edit" size={20} color={'#000000'} />
+            </TouchableOpacity>
             
+            {/* Profile Image */}
+            <Image source={basicProfile} style={styles.profileImage} />
+            
+            {/* Username */}
+            <Text style={styles.username}>username</Text>
+            
+            {/* User Info */}
+            <View style={styles.userInfoArea}>
+                <View><Text>글</Text><Text>num</Text></View>
+                <View><Text>댓글</Text><Text>num</Text></View>
+                <View><Text>여행</Text><Text>num</Text></View>
+            </View>
+            
+            {/* Edit Mode */}
+            {isEdit && (
+                <TouchableOpacity onPress={() => setIsEdit(false)}>
+                    <Text>수정</Text>
+                </TouchableOpacity>
+            )}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        padding: 20,
+        width: deviceWidth - 30,
+        alignSelf: 'center',
+        marginTop: 20,
+        gap: 20,
+        height: 310,
+        ...Platform.select({
+            ios: {
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        backgroundColor: "#ffffff",
+        alignItems: 'center',
+        // position: 'relative',
+    },
+    editButton: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+    },
+    profileImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+    },
+    username: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000000',
+    },
+    userInfoArea: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 60,
+    },
+});
 
 export default MyProfile;
--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import MyProfile from '../components/myPage/MyProfile';
 
 const ProfileScreen: React.FC = () => {
     return (
         <ScrollView style={styles.screen}>
-            
+            <MyProfile/>
         </ScrollView>
     );
 };
