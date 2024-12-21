56049cdfa58a28165b297e4cbee2863f44baadb6
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-12-22 03:43:57 +0900

    Feat: Ai에게 여행 평가받기 프로토타입

--- app/screens/TravelAiScreen.tsx
+++ app/screens/TravelAiScreen.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { travelDetail } from '../types/types';
+import axios from 'axios';
+import { urls } from '../utils/requests';
 
 interface TravelAiScreenProps {
     travelDetail: travelDetail;
 }
 const TravelAiScreen: React.FC<TravelAiScreenProps> = ({travelDetail}) => {
+    const [loading, setLoading] = useState<boolean>(false);
     const {
         createdAt, // e.g., "2024-12-15T16:16:26.000+00:00"
         deletedAt, // e.g., null or "2024-12-15T16:16:26.000+00:00"
@@ -21,7 +24,19 @@ const TravelAiScreen: React.FC<TravelAiScreenProps> = ({travelDetail}) => {
         usernames,
     } = travelDetail;
 
-    const get
+    const getAIRate = async() => {
+        try{
+            setLoading(true);
+            const response =await axios.get(urls.aiCheck(travelCode));
+            console.log(response.data);
+        }
+        catch(err){
+            console.error(err);
+        }
+        finally{
+            setLoading(false);
+        }
+    }
     return (
         <View>
             
